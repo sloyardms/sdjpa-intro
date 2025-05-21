ea0e1647866c4ca0630d1358a882aaace368bf33
sloyardms <sloyard_ms@outlook.com>
2025-05-21 13:29:37 -0700

    Initial commit

---
+++ src/main/java/com/sloyard/sdjpaintro/SdjpaintroApplication.java
@@ -0,0 +1,13 @@
+package com.sloyard.sdjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaintroApplication.class, args);
+	}
+
+}
