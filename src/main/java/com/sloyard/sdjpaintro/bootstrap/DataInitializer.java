625b83e4d1b83c97210b98e6379a66b5e6f1ef2b
sloyardms <sloyard_ms@outlook.com>
2025-05-22 15:38:15 -0700

    new profile annotation for dataInitializer class

--- src/main/java/com/sloyard/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/com/sloyard/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package com.sloyard.sdjpaintro.bootstrap;
 import com.sloyard.sdjpaintro.domain.Book;
 import com.sloyard.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({ "local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -16,16 +18,12 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+
         Book bookDDD = new Book(null, "Domain Driven Design", "978-0321125215", "Addison-Wesley");
-
-        System.out.println("Id: "+bookDDD.getId());
-
         Book savedDDD = bookRepository.save(bookDDD);
 
-        System.out.println("Id: "+savedDDD.getId());
-
         Book bookSIA = new Book(null, "Software Architecture in Practice", "978-0134494166", "Addison-Wesley");
-
         Book savedSIA = bookRepository.save(bookSIA);
 
         bookRepository.findAll().forEach(book->{
