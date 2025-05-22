1ae575b11d97475c43fb7f6659bf61dace005ce5
sloyardms <sloyard_ms@outlook.com>
2025-05-21 18:25:06 -0700

    spring test transactions and commit

--- src/test/java/com/sloyard/sdjpaintro/SdjpaintroApplicationTests.java
+++ src/test/java/com/sloyard/sdjpaintro/SdjpaintroApplicationTests.java
@@ -1,6 +1,7 @@
 package com.sloyard.sdjpaintro;
 
 import com.sloyard.sdjpaintro.repositories.BookRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,7 @@ class SdjpaintroApplicationTests {
 	void testBookRepository() {
 	 	long count = bookRepository.count();
 
-		 assert count > 0;
+		Assertions.assertThat(count).isGreaterThan(0);
 	}
 
 	@Test
--- src/test/java/com/sloyard/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/com/sloyard/sdjpaintro/SpringBootJpaTestSlice.java
@@ -2,24 +2,44 @@ package com.sloyard.sdjpaintro;
 
 import com.sloyard.sdjpaintro.domain.Book;
 import com.sloyard.sdjpaintro.repositories.BookRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     BookRepository bookRepository;
 
+    //Commit is to make test2 see the data persisted by test1
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSlice(){
-        long count = bookRepository.count();
+        long countBefore = bookRepository.count();
+        Assertions.assertThat(countBefore).isEqualTo(0);
 
         bookRepository.save(new Book(null, "My Book", "978-0321125215", "Self"));
 
         long countAfter = bookRepository.count();
 
-        assert countAfter > count;
+        Assertions.assertThat(countAfter).isGreaterThan(countBefore);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSliceTransaction(){
+        long countBefore = bookRepository.count();
+        Assertions.assertThat(countBefore).isEqualTo(1);
+
+
     }
 }
